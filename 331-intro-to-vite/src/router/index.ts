8a231d921d38eeafd0b95a2f8ee405caa20e6912
Cai Jialu <2110220161@qq.com>
2024-11-05 21:02:56 +0800

    3.1

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -82,6 +82,13 @@ const router = createRouter({
       name: 'not-found',
       component: NotFoundView,
     },
+    scrollBehavior(to, from, savedPosition) ,
+          if (savedPosition) {
+            return savedPosition
+          } else ,
+            return { top: 0 }
+          }
+       }
     {
       path: '/students',
       name: 'students',
--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -9,7 +9,7 @@ import EventService from '@/services/EventService'
 const events = ref<Event[] | null>(null)
 const totalEvents = ref(0)
 const hasNexPage = computed(() => {
-  const totalPages = Math.ceil(totalEvents.value / 2)
+  const totalPages = Math.ceil(totalEvents.value / 3)
   return page.value < totalPages
 })
 
@@ -29,8 +29,8 @@ const size = computed(() => props.size)
 
 onMounted(() => {
   watchEffect(() => {
-    events.value = null
-    EventService.getEvents(size.value, page.value)
+   
+    EventService.getEvents(3, page.value)
       .then(response => {
         events.value = response.data
         totalEvents.value = response.headers['x-total-count']
